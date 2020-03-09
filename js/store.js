68015c90313d82328f7496e7091e86016102b731
Majd Imar <majd2012995@gmail.com>
2020-03-09 17:09:19 +0200

    added store.css file

--- js/store.js
+++ js/store.js
@@ -1,6 +1,6 @@
 var Names = ['Ahmed', 'Baraa', 'Bashar', 'Esraa', 'Majd', 'Osama', 'Tasneem', 'Razan', 'Reham', 'Samer', 'Leen', 'Shihab'];
 var Price = ['10$', '15$', '20$', '12$', '8$', '17$', '13$', '18$', '25$', '30$', '22$', '7$'];
-
+var chosenProduct = [];
 
 function Items(name, price) {
     this.name = name;
@@ -21,11 +21,12 @@ function render() {
     for (var j = 0; j < Names.length; j++) {
         var product = document.createElement('div');
         container.appendChild(product);
-        product.setAttribute('id', Items.all[j].name);
+        
         var image = document.createElement('img');
         product.appendChild(image);
         image.setAttribute('src', Items.all[j].imagePath);
         image.setAttribute('alt', Items.all[j].name);
+        image.setAttribute('id', Items.all[j].name);
         var kidsName = document.createElement('p');
         product.appendChild(kidsName);
         kidsName.textContent =`Done by: ${Items.all[j].name}.`;
@@ -36,3 +37,32 @@ function render() {
     }
 }
 render();
+
+var container = document.getElementById('container');
+
+container.addEventListener('click' , choseProduct);
+
+function choseProduct (event) {
+if (event.target.id !== 'container' ){
+    console.log('wtt');
+    
+
+    for (var x = 0 ; x < Names.length ; x++ ) {
+
+        if(event.target.id == `${Items.all[x].name}`){
+
+          if(!chosenProduct.includes(Items.all[x])){
+
+                chosenProduct.push(Items.all[x]);
+                updates();     
+            }
+        }
+    }
+}
+}
+
+function updates (){
+    var localProducts = JSON.stringify(chosenProduct);
+    localStorage.setItem('localchosenProducts',localProducts);
+}
+
