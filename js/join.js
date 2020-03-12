dba7289dd1d34653cdc08dde4703e5f0c2bd3767
EsraaMamoun <esraamamoun5@gmail.com>
2020-03-12 11:18:30 +0200

    join-us js

---
+++ js/join.js
@@ -0,0 +1,13 @@
+function clickJoin() {
+    var Email = document.getElementById('email').value;
+    var firstName = document.getElementById('name').value;
+    var lastName = document.getElementById('name2').value;
+
+    var messageHeader = document.getElementById('messageHeader');
+    var content = document.getElementById('content');
+
+    messageHeader.textContent = `Welcome ${firstName} ${lastName}.`;
+    content.textContent = `We have sent you an e-mail to (${Email}) to activate the site, please open the link attached to the message to complete your joining our family.`;
+}
+
+    // window.open("../index.html" , "_self");
